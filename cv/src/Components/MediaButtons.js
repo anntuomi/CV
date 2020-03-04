57d2383f9c887b3ab61e62c68cea92016f0e181d
anntuomi <anntuomi@gmail.com>
2020-03-05 00:14:11 +0200

    Fixed a target _blank security issue in media buttons

--- cv/src/Components/MediaButtons.js
+++ cv/src/Components/MediaButtons.js
@@ -4,8 +4,8 @@ import github from './github_logo.png'
 const MediaButtons = () => {
     return (
       <div>
-      <a className="button_media" href="https://linkedin.com/in/anna-tuominen-4382aa8a" target="_blank" title="Linkedin">in</a>
-      <a className="button_media" href="https://github.com/anntuomi" target="_blank" title="Github"><img src={github} alt='Git' /></a>
+      <a className="button_media" href="https://linkedin.com/in/anna-tuominen-4382aa8a" target="_blank" rel="noopener noreferrer" title="Linkedin">in</a>
+      <a className="button_media" href="https://github.com/anntuomi" target="_blank" rel="noopener noreferrer" title="Github"><img src={github} alt='Git' /></a>
       </div>
     )
   }
